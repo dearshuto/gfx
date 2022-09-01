7eea500c4491f81622a9ee048a59eae36055a3af
Shuto Shikama <@gmail.com>
2022-09-02 00:36:58 +0900

    [WIIP] Vulkan 実装

---
+++ Include/sj/gfx/vk/gfx_DeviceVk.h
@@ -0,0 +1,24 @@
+#ifndef gfx_DeviceVk_h
+#define gfx_DeviceVk_h
+
+#include <vulkan/vulkan.hpp>
+
+namespace sj::gfx {
+class DeviceInfo;
+}
+
+namespace sj::gfx::vk {
+
+class DeviceVk
+{
+public:
+    void Initialize(const DeviceInfo& deviceInfo) noexcept;
+
+private:
+    VkInstance m_Instance;
+    VkDevice m_Device;
+};
+
+}  // namespace sj::gfx::vk
+
+#endif /* gfx_DeviceVk_h */
---
+++ Sources/Libraries/gfx/vk/gfx_DeviceVk.cpp
@@ -0,0 +1,12 @@
+#include <sj/gfx/vk/gfx_DeviceVk.h>
+
+#include <vulkan/vulkan.hpp>
+
+namespace sj::gfx::vk {
+
+void DeviceVk::Initialize(const DeviceInfo &deviceInfo) noexcept
+{
+    VkInstanceCreateInfo instanceCreateInfo;
+}
+
+}  // namespace sj::gfx::vk

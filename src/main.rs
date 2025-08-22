c3f49fb0f2849b64c1532650e367e7d56fb71a55
Shuto Shikama <shuto.shikama@gmail.com>
2025-08-22 21:02:51 +0900

    fixup! [WIP] デモ移植

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::ops::Deref;
-
-use eframe::run_native;
-
 fn main() {
-    let options = eframe::NativeOptions::default();
-
-    eframe::run_native("gfx", options, Box::new(|cc| Ok(Box::new(App::new(cc))))).unwrap();
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        let options = eframe::NativeOptions::default();
+        eframe::run_native("gfx", options, Box::new(|cc| Ok(Box::new(App::new(cc))))).unwrap();
+    }
 }
 
 struct App {}
@@ -14,7 +12,7 @@ impl App {
     pub fn new(cc: &eframe::CreationContext) -> Self {
         let wgpu_state = cc.wgpu_render_state.as_ref().unwrap();
         let entry = unsafe { ash::Entry::from_static_fn(vknew::wgpu::get_static_fn()) };
-        let _info = vknew::wgpu::WasmCompatibilityCreateInfo::from_instance(
+        let info = vknew::wgpu::WasmCompatibilityCreateInfo::from_instance(
             wgpu::Instance::default(),
             wgpu_state.device.clone(),
             wgpu_state.queue.clone(),
@@ -26,7 +24,7 @@ impl App {
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         eframe::egui::SidePanel::left("Left").show(ctx, |ui| {
             if ui.button("Hello World").clicked() {
                 println!("Clicked");
@@ -35,13 +33,14 @@ impl eframe::App for App {
     }
 }
 
+#[allow(unused)]
 struct Triangle;
 impl eframe::egui_wgpu::CallbackTrait for Triangle {
     fn paint(
         &self,
-        info: eframe::egui::PaintCallbackInfo,
+        _info: eframe::egui::PaintCallbackInfo,
         render_pass: &mut wgpu::RenderPass<'static>,
-        callback_resources: &eframe::egui_wgpu::CallbackResources,
+        _callback_resources: &eframe::egui_wgpu::CallbackResources,
     ) {
         vknew::wgpu::interoperability::push_draw_command(
             render_pass,
